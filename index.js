0bbb0fc21759600e9f5e73ab1370b86dd944b2c4
Marcal Ramos <suncastelo@gmail.com>
2019-03-24 19:20:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function add( a, b){
+  return a + b
+}
+
+add(2, 2 )
+
+function subtract( a, b ){
+  return a - b
+}
+
+subtract( 5, 6)
+
+function multiply( a, b){
+  return a * b
+}
+
+multiply(10,2)
+
+function divide( a, b){
+  return a / b 
+}
+ divide(8,5)
+ 
+ function inc(n){
+   var inc
+   return n + 1
+ }
+ 
+ inc(10)
